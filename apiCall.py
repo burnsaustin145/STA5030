fddaed7cf56c21125456699235544f48a9e6b8b0
burnsaustin145 <burnsaustin145@yahoo.com>
2023-04-24 14:52:04 -0400

    increasing api gulp size for 500

--- apiCall.py
+++ apiCall.py
@@ -6,6 +6,9 @@ api_key = "qxwn52dqxvaqkfuf9z2fsfbb"
 season_year = "2021"
 season_phase = "REG"
 
+# the number of total api calls to make
+intake = 500
+
 # Get the list of games in the season
 games_url = f"https://api.sportradar.us/nba/trial/v8/en/games/2021/REG/schedule.json?api_key={api_key}"
 games_response = requests.get(games_url)
@@ -26,7 +29,7 @@ for game in games_data["games"]:
     except:
         boxscore_data = "null no data this time"
     count += 1
-    if count > 200:
+    if count > intake:
         break
     # Do something with the boxscore_data, such as
 
@@ -34,8 +37,11 @@ for game in games_data["games"]:
     # f = open("demofile2.txt", "a")
     # f.write(pprint.pformat(boxscore_data))
     # f.close()
+
+# changing to get more data
+jar_name = "pickledDat500.pickle"
 try:
-    with open('pickledDat200.pickle', 'wb') as handle:
+    with open(jar_name, 'wb') as handle:
         pickle.dump(allDat, handle, protocol=pickle.HIGHEST_PROTOCOL)
 except:
     print("can't pickle")
--- main.py
+++ main.py
@@ -42,8 +42,32 @@ def writeCSV2(gameIds, dat, toFile, write, labels, statsKeys):
             write.writerow(currRow)
     toFile.close()
 
+def writeCSV3(gameIds, dat, toFile, write, labels, statsKeys):
+    labels.append("home_away")
+    labels.append("capacity")
+    write.writerow(labels)
+    for gameId in gameIds:
+        for location in ("home", "away"):
+            currRow = [gameId]
+            teamPts = dat[gameId][location]['points']
+            otherPts = dat[gameId]['home' if location == 'away' else 'away']['points']
+            currRow.append(teamPts)
+            currRow.append('win' if teamPts > otherPts else 'loss')
+            try:
+                currRow.append(dat[gameId]['attendance'])
+            except:
+                currRow.append("no_info")
+            for stat in statsKeys:
+                try:
+                    currRow.append(dat[gameId][location]['statistics'][stat])
+                except:
+                    currRow.append("NA")
+            currRow.append(location)
+            currRow.append(dat[gameId]["venue"]["capacity"])
+            write.writerow(currRow)
+    toFile.close()
 
-def generate_initial_labels():
+def generate_initial_labels(dat):
     gameIds = list(dat.keys())
     print("got ids")
     statsKeys = list(dat[gameIds[0]]['home']['statistics'])
@@ -67,31 +91,52 @@ def generate_vars1(statsKeys, gameIds):
 
 
 # this generate funciton adds home/away variable (should refactor to select for multiple vars??
-def generate_vars2(statsKeys, gameIds):
-    with open('tidy200_conf.csv', mode='w') as toFile:
+def generate_vars2(statsKeys, gameIds, csv_name):
+    with open(csv_name, mode='w') as toFile:
         write = csv.writer(toFile, delimiter=',', quotechar='"')
         columnLabels = ['Game Id', 'points', 'result', 'attendance']
         for lab in statsKeys:
             columnLabels.append(lab)
         writeCSV2(gameIds, dat, toFile, write, columnLabels, statsKeys)
 
+def generate_vars3(statsKeys, gameIds, csv_name):
+    with open(csv_name, mode='w') as toFile:
+        write = csv.writer(toFile, delimiter=',', quotechar='"')
+        columnLabels = ['Game Id', 'points', 'result', 'attendance']
+        for lab in statsKeys:
+            columnLabels.append(lab)
+        writeCSV3(gameIds, dat, toFile, write, columnLabels, statsKeys)
 
-# some initial testing
-if __name__ == "__main__":
+
+def unPickle(jar_name):
     try:
-        with open('pickledDat200.pickle', 'rb') as handle:
+        with open(jar_name, 'rb') as handle:
             dat = pickle.load(handle)
         print("got dat")
+        return dat
     except:
         print("open pickle jar error")
 
-# %% creating tidy200.csv for initial testing
-# keys, gameIds = generateInitialLabels()
-# varSet1(keys, gameIds)
+# some initial testing
+if __name__ == "__main__":
 
-# adding home/away stat to data
-keys, game_ids = generate_initial_labels()
-generate_vars2(keys, game_ids)
+    #dat = unPickle('pickledDat200.pickle')
+
+    # %% creating tidy200.csv for initial testing
+    # keys, gameIds = generateInitialLabels()
+    # varSet1(keys, gameIds)
+
+    # adding home/away stat to data
+    # keys, game_ids = generate_initial_labels(dat)
+    # generate_vars2(keys, game_ids, "tidy200_conf.csv")
+
+    # adding capacity to data
+    # generate_vars3(keys, game_ids, "tidy200_conf_true.csv")
+
+    # getting more data than everrr
+    dat = unPickle('pickledDat500.pickle')
+    keys, game_ids = generate_initial_labels(dat)
+    generate_vars3(keys, game_ids, "tidy500_conf_true.csv")
 
 
 
